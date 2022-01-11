820d0ef234caeb70b217c2822d2e7d643079a930
Daniel Verdu <iamadeveloper.es@gmail.com>
2022-01-11 23:38:02 +0100

    Creo componente y vista demo AppInputField

---
+++ src/components/ui/input-field/AppInputField.vue
@@ -0,0 +1,85 @@
+<template>
+  <div 
+  class="app-input-wrapper floating-label"
+  :class="(isfloatingLabel && floatingLabel) || value ? 'is-focused' : ''"
+  >
+    <input
+    class="app-input app-input-field"
+    :disabled="disabled"
+    :type="inputType"
+    :placeholder="placeholderLabel"
+    :required="required"
+    :readonly="readOnly"
+    :aria-label="placeholderLabel || label"
+    :name="inputRef"
+    :id="inputRef"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+    @focus="floatingActive()"
+    @blur="floatingRemove()"
+    />
+    <label 
+    class="app-label"
+    :for="inputRef">{{ label }}</label>
+  </div>
+</template>
+<script>
+export default {
+  name: 'app-input-field',
+  props: {
+    inputType: String,
+    label: {
+      type: String,
+      default: "",
+    },
+    placeholderLabel: {
+      type: String,
+      default: "",
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    inputRef: {
+      type: String,
+      default: "",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    readOnly: {
+      type: Boolean,
+      default: false,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+    floatingLabel:{
+      type: Boolean,
+      default: false
+    }
+  },
+  data(){
+    return{
+      isfloatingLabel: false,
+      isLabel: false,
+    }
+  },
+  methods:{
+    floatingActive(){
+      this.isfloatingLabel = true
+    },
+    floatingRemove(){
+      this.isfloatingLabel = false
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+
+.app-input-field[readonly] {
+  cursor: no-drop;
+}
+</style>
---
+++ src/components/ui/input-field/demo/AppInputFieldDemo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app-input-field-demo">
+    <app-input-field
+    :label="'Input field'"
+    :floatingLabel="true"
+    v-model="inputValue"
+    ></app-input-field>
+    <div class="output mt-3">
+        <span>{{inputValue}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppInputField from '../AppInputField.vue'
+export default {
+    name: 'app-input-field-demo', 
+    components: { AppInputField },
+    data(){
+        return{
+            inputValue: ''
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
