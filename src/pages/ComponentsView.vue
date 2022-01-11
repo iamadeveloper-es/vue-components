6fc90ca206df2e5e742472a52ae9bce81d271d0b
Daniel Verdu <iamadeveloper.es@gmail.com>
2022-01-11 23:34:20 +0100

    Creo página ComponentsView como plantilla para renderizar los componentes

---
+++ src/pages/ComponentsView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <div class="app-info-component mb-6">
+          <h1 class="mb-4">Componente</h1>
+          <h2>{{setComponentName}}</h2>
+      </div>
+      <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'components-view',
+    computed:{
+        setComponentName(){
+            const routeName = this.$route.name
+            const componentName = routeName.substring(0, routeName.length - 4);
+            return componentName
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
