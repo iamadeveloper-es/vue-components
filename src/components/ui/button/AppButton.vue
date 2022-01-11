2474296b0bf1347a62cdf9bb07b717b741726839
Daniel Verdu <iamadeveloper.es@gmail.com>
2022-01-11 23:37:16 +0100

    Creo componente y vista AppButton

---
+++ src/components/ui/button/AppButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <button 
+  class="app-button"
+  :type="btnType"
+  :disabled="disabled"
+  @click="handleEvent($event)"
+  >
+      {{btnText}}
+  </button>
+</template>
+
+<script>
+export default {
+    name: 'app-button',
+    props: {
+        btnText:{
+            type: String
+        },
+        btnType:{
+            type: String,
+            default: 'button'
+        },
+        disabled:{
+            type: Boolean,
+            default: false
+        }
+    },
+    methods:{
+        handleEvent(ev){
+            this.$emit('clicked', ev)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.app-button{
+    background-color: #2d86cf;
+    color: white;
+    display: inline-block;
+    padding: 14px 10px;
+    border-radius: 3px;
+    border: none;
+    line-height: 1.4;
+    text-align: center;
+    cursor: pointer;
+    &:hover{
+        opacity: .9;
+    }
+}
+</style>
---
+++ src/components/ui/button/demo/AppButtonDemo.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="app-button-demo">
+      <app-button
+      :btnText="btnData.label"
+      @clicked="counterIncrement($event)"
+      ></app-button>
+      <div class="mt-3">
+          <span>{{`clicked ${clickedTimes} times`}}</span>
+      </div>
+  </div>
+</template>
+
+<script>
+import AppButton from '../AppButton.vue'
+export default {
+  components: { AppButton },
+    name: 'app-button-demo',
+    data(){
+        return{
+            btnData:{
+                label: 'I am a button',
+            },
+            clickedTimes: 0
+        }
+    },
+    methods:{
+        counterIncrement(){
+            this.clickedTimes++
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.app-button{
+    background-color: var(--primary-blue);
+    color: white;
+    display: inline-block;
+    padding: 14px 10px;
+    border-radius: 3px;
+    border: none;
+    line-height: 1.4;
+    text-align: center;
+    cursor: pointer;
+    &:hover{
+        opacity: .9;
+    }
+}
+</style>
